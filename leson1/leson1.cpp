6ed7d45b81170a9dcabd4003fcd7e54f35979560
welsyyy <151341633+welsyyy@users.noreply.github.com>
2024-09-04 17:44:48 +0300

    повторныйпервый комитт

--- leson1/leson1.cpp
+++ leson1/leson1.cpp
@@ -1,1 +1,27 @@
-﻿
+﻿#include<iostream>
+
+#include<windows.h>
+
+#include<string>
+
+#include<string.h>
+
+#include<fstream>
+
+using namespace std;
+
+int main()
+
+{
+
+	SetConsoleOutputCP(1251);
+
+	SetConsoleCP(1251);
+
+	setlocale(LC_ALL, "RU");
+
+	srand(time(NULL));
+
+	return 0;
+
+}
